25ea28c1f2f0576cc551d6cba9582dc638d1650e
Guillaume Isabelle <jgi@jgwill.com>
2020-04-13 16:57:07 -0400

    imported Docker LAMP

---
+++ www/index.php
@@ -0,0 +1,6 @@
+<h1>Hey, that is JGWill's Stack.</h1>
+<h2>What else? no your business, yet ;) </h2>
+<hr>
+
+<script src="https://liberapay.com/jgwill/widgets/button.js"></script>
+<noscript><a href="https://liberapay.com/jgwill/donate"><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg"></a></noscript>
